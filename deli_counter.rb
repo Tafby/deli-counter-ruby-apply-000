c4b0999b6d2dd3b234464a54c9091ce5c6728bdf
Tiffany Carter <tafby88@gmail.com>
2018-09-22 16:10:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else 
-    array.each_with_index do |name, index|
+    array.each_with_index do |name, index +1|
     puts "The line is currently: #{index}. #{name} "
   end
   end
