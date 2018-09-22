8046a5d596144024caaa71ed6d6091cd8d307b49
Tiffany Carter <tafby88@gmail.com>
2018-09-22 16:41:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 def take_a_number(deli, name)
   deli.push(name)
   number = deli[-1]
-  string = "Welcome #{name}. You are number" + number + "."
+  puts "Welcome #{name}. You are number" + number + "."
 end
 
 def now_serving(deli)
